36e540dacf7b3c0640ea3fe9ac281788d92ea29c
Sharif Khan <sharifkhan74647464@gmail.com>
2024-07-27 11:19:36 +0600

    loading functionality

--- src/components/ConnectWithBackend/CancellingRequest.jsx
+++ src/components/ConnectWithBackend/CancellingRequest.jsx
@@ -4,16 +4,23 @@ import axios, { CanceledError } from "axios";
 const CancellingRequest = () => {
   const [users, setUser] = useState([]);
   const [errors, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const controller = new AbortController();
+
+    setLoading(true);
     axios
       .get("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
-      .then((res) => setUser(res.data))
+      .then((res) => {
+        setUser(res.data);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
@@ -21,6 +28,7 @@ const CancellingRequest = () => {
   return (
     <>
       {errors && <p className="text-red-800">{errors}</p>}
+      {loading && <p>loading...</p>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
